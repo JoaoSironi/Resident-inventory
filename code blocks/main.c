029ad11313757092799f4bffec269588df3e68e0
JoaoSironi <joaovsironi@gmail.com>
2019-07-20 23:00:07 -0300

    adicionado a funcionalidade de remover itens

--- code blocks/main.c
+++ code blocks/main.c
@@ -7,17 +7,21 @@ int tres8tao();
 int calibre12();
 int municao38();
 int municao12();
+int removetres8tao();
+int removecalibre12();
+int removemunicao38();
+int removemunicao12();
 int arsenal();
 int limpar();
 
-int inventory[5][10];
+int inventory[4][9];
 	
 int main() {
 	int i=0;
 	int j=0;
 	
-		for (i=0;i<=5;i++){
-			for (j=0;j<=10;j++){	
+		for (i=0;i<=4;i++){
+			for (j=0;j<=9;j++){	
 			inventory[i][j]=0;
 			}
 		}	
@@ -74,7 +78,16 @@ int inicio() {
 			printf("	4| Municao 12 \n");
 			scanf("%d",&remover);
 			system("cls");
-		}
+			
+			switch(remover){
+				case 1: removetres8tao();break;
+				case 2: removecalibre12();break;
+				case 3: removemunicao38();break;
+				case 4: removemunicao12();break;
+				default: printf("OPCAO INVALIDA \n");
+				system("pause");
+			}
+		}			
 					
 		if(opcao == 3){
 			system("cls");
@@ -90,27 +103,25 @@ int inicio() {
 			
 	}while(flag==0);
 }
-
-
 int arsenal(){
 	int flag=0;
 	int i=0;
 	int j=0;
 	
-	for(i=0;i<=5;i++){
+	for(i=0;i<=4;i++){
 		printf("\n");				
-		for(j=0;j<=10;j++){
+		for(j=0;j<=9;j++){
 			printf(" %d ",inventory[i][j]);
 		}
 	}
-	printf("\nPara limparo arsenal digite 4 \n");
+	printf("\n\n\nPara limparo arsenal digite 4 \n");
 	printf("Para voltar  ao menu digite 0 \n");
 	scanf("%d",&flag);
-	
-	if (flag == 4)
-		limpar();
-	if(flag==0)
-		inicio();	
+	switch(flag){
+		case 4: limpar();break;
+		case 0: inicio();break;
+		default: printf("OPCAO INVALIDA");
+	}	
 }
 int limpar(){
 	int i=0;
@@ -127,34 +138,83 @@ int limpar(){
 int tres8tao() {
 	int i=0;
 	int j=0;
-	for (i=0;i<=3;i++){
-		for (j=0;j<=2;j++){	
+	for (i=0;i<=2;i++){
+		for (j=0;j<=1;j++){	
+		if(inventory == 1 || inventory == 2 || inventory == 3 || inventory == 4 ){
+			i++;
+			j++;
+		} else{
+			inventory[i][j]=1;
 		}
 		
-	}
-}
-int municao38() {
-	int i=0;
-	int j=0;
-	for (i=0;i<=2;i++){
-		for ( j=0;j<=1;j++){		
-		}	
+		}
+		
 	}
 }
 int calibre12() {
 	int i=0;
 	int j=0;
-	for (i=0;i<=3;i++){
-		for (j=0;j<=7;j++){		
+	for (i=0;i<=2;i++){
+		for (j=0;j<=6;j++){	
+		inventory[i][j]=2;	
+		}	
+	}
+}
+int municao38() {
+	int i=0;
+	int j=0;
+	for (i=0;i<=1;i++){
+		for ( j=0;j<=0;j++){
+		inventory[i][j]=3;		
 		}	
 	}
 }
 int municao12() {
 	int i=0;
 	int j=0;
-	for (i=0;i<=2;i++){
-		for (j=0;j<=1;j++){	
+	for (i=0;i<=1;i++){
+		for (j=0;j<=0;j++){	
+		inventory[i][j]=4;
 		}	
 	}
 }
-
+int removetres8tao(){
+	int i=0;
+	int j=0;
+	for (i=0;i<=4;i++){
+		for (j=0;j<=9;j++){
+			if(inventory[i][j]==1)	
+				inventory[i][j]=0;	
+		}	
+	}
+}
+int removecalibre12(){
+	int i=0;
+	int j=0;
+	for (i=0;i<=4;i++){
+		for (j=0;j<=9;j++){
+			if(inventory[i][j]==2)	
+				inventory[i][j]=0;	
+		}	
+	}
+}
+int removemunicao38(){
+	int i=0;
+	int j=0;
+	for (i=0;i<=4;i++){
+		for (j=0;j<=9;j++){
+			if(inventory[i][j]==3)	
+				inventory[i][j]=0;	
+		}	
+	}
+}
+int removemunicao12(){
+	int i=0;
+	int j=0;
+	for (i=0;i<=4;i++){
+		for (j=0;j<=9;j++){
+			if(inventory[i][j]==4)	
+				inventory[i][j]=0;	
+		}	
+	}
+}
