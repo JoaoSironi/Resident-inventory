436ed1f7e251a66c1d84ca02700356b815c4a98b
JoaoSironi <joaovsironi@gmail.com>
2019-07-20 22:13:17 -0300

    adicionado a opção limpar, criação da função inicio() e limpezado arsenal

--- code blocks/main.c
+++ code blocks/main.c
@@ -2,37 +2,50 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int inicio();
 int tres8tao();
 int calibre12();
 int municao38();
 int municao12();
+int arsenal();
+int limpar();
 
+int inventory[5][10];
+	
 int main() {
-	int inventory[5][10];
-	int flag=0;
-	int opcao=0;
-	int adicionar=0;
-	int remover=0;
 	int i=0;
 	int j=0;
 	
-	system("color 74");
+		for (i=0;i<=5;i++){
+			for (j=0;j<=10;j++){	
+			inventory[i][j]=0;
+			}
+		}	
 	
-	printf("INVENTORIO DE ARMAMENTO \n");
-	system("pause");
-	system("cls");
-	
+		system("color 74");
+		
+		printf("INVENTORIO DE ARMAMENTO \n");
+		system("pause");
+		system("cls");
+		inicio();
+		return 0;
+}
+int inicio() {
+	int flag=0;	
+	int opcao=0;
+	int adicionar=0;
+	int remover=0;	
+		
 	do{
 		system("cls");
 		system("color 04");
 		printf("	1| Adiconar \n");
 		printf("	2| Remover \n");
-		print("		3| Mostar arsenal");
+		printf("	3| Mostar arsenal \n");
 		printf("insira o opcao que deseja executar: ");
 		scanf("%d",&opcao);
-
-		if(opcao == 1){
 			
+		if(opcao == 1){			
 			system("cls");
 			printf("selecione item que deseja adicionar \n");
 			printf("	1| Revolver 38 \n");
@@ -41,7 +54,7 @@ int main() {
 			printf("	4| Municao 12 \n");
 			scanf("%d",& adicionar);
 			system("cls");
-		
+					
 			switch(adicionar){
 				case 1: tres8tao();break;
 				case 2: calibre12();break;
@@ -51,9 +64,8 @@ int main() {
 				system("pause");
 			}
 		}
-	
-		if(opcao == 2){
 			
+		if(opcao == 2){			
 			system("cls");
 			printf("selecione item que deseja remover \n");
 			printf("	1| Revolver 38 \n");
@@ -63,27 +75,55 @@ int main() {
 			scanf("%d",&remover);
 			system("cls");
 		}
-		
+					
 		if(opcao == 3){
 			system("cls");
-			for(i=0;i<=5;i++){
-				for(j=0;j<=10;j++){
-					printf("%d",inventory[i][j]);
-				}
-			}
+			arsenal();
+			system("pause");
 		}
-		
-		if(opcao != 1 && opcao != 2){
+					
+		if(opcao != 1 && opcao != 2 && opcao != 3){
 			printf("OPCAO INVALIDA, TENTE NOVAMENTE!! \n");	
 			system("pause");
 			system("cls");
 		}
-
+			
 	}while(flag==0);
-	return 0;
 }
 
 
+int arsenal(){
+	int flag=0;
+	int i=0;
+	int j=0;
+	
+	for(i=0;i<=5;i++){
+		printf("\n");				
+		for(j=0;j<=10;j++){
+			printf(" %d ",inventory[i][j]);
+		}
+	}
+	printf("\nPara limparo arsenal digite 4 \n");
+	printf("Para voltar  ao menu digite 0 \n");
+	scanf("%d",&flag);
+	
+	if (flag == 4)
+		limpar();
+	if(flag==0)
+		inicio();	
+}
+int limpar(){
+	int i=0;
+	int j=0;
+	
+	system("cls");
+	for (i=0;i<=5;i++){
+		for (j=0;j<=10;j++){	
+		inventory[i][j]=0;
+		}
+	}
+	arsenal();
+}
 int tres8tao() {
 	int i=0;
 	int j=0;
